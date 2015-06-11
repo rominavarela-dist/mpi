a4df515dc3b03642bb62b6de868f753130a6323b
rominavarela <rominavarela.mx@gmail.com>
2015-06-11 12:40:20 -0500

    simple hello

---
+++ OpenMPIHello/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <mpi.h>
+#define MASTER 0
+
+using namespace std;
+
+//nProcess, thisN, thisName
+int world_size,world_rank,name_len;
+char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+/**
+* @method main
+**/
+int main(int argc, char* argv[])
+{
+  	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Get_processor_name(processor_name, &name_len);
+
+  	//init
+  	MPI_Barrier(MPI_COMM_WORLD);
+    		std::cout << "[" << world_rank << "]" << "HELLO" << endl;
+
+  	MPI_Finalize();
+  	return 0;
+}
