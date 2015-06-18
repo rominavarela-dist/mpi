c7bd93a2648c0297a01c0080c79d7a2989c9bf46
rominavarela <rominavarela.mx@gmail.com>
2015-06-18 09:01:29 -0500

    spawn from node node with new host_file

--- Spawn/master_src/master.cpp
+++ Spawn/master_src/master.cpp
@@ -13,37 +13,37 @@ MPI_Comm children;
 char processor_name[MPI_MAX_PROCESSOR_NAME];
 int name_len;
 
-void master_work();
+void master_work()
+{
+  std::cout << "[" << rank << "]" << "HELLO FROM MASTER @ " << processor_name << endl;
+  MPI_Recv(&n_children, 1, MPI_INT, 0, 0, children, MPI_STATUS_IGNORE);
+  std::cout << "[" << rank << "]" << " Got " << n_children << " slaves..." << endl;
+  std::cout << "[" << rank << "]" << "BYE BYE FROM MASTER" << endl;
+}
 
 /**
 * @method main
 **/
 int main(int argc, char* argv[])
 {
+  //init
   MPI_Init(&argc, &argv);
   rank = MPI::COMM_WORLD.Get_rank();
   MPI_Get_processor_name(processor_name, &name_len);
 
-  MPI_Comm_spawn("slave", argv, 4,
-      MPI_INFO_NULL, 0, MPI_COMM_WORLD,
+  //add hosts and spawn
+  MPI_Info info;
+  MPI_Info_create(&info);
+  MPI_Info_set(info,"add-hostfile","host_file");
+  MPI_Comm_spawn("slave", MPI_ARGV_NULL, 4,
+      info, 0, MPI_COMM_WORLD,
       &children, MPI_ERRCODES_IGNORE);
 
+  //work
   master_work();
-
   sleep(3);
 
+  //finialize
   MPI_Finalize();
-
   return 0;
-}
-
-void master_work()
-{
-  std::cout << "[" << rank << "]" << "HELLO FROM MASTER @ " << processor_name << endl;
-
-  MPI_Recv(&n_children, 1, MPI_INT, 0, 0, children, MPI_STATUS_IGNORE);
-
-  std::cout << "[" << rank << "]" << " Got " << n_children << " slaves..." << endl;
-
-  std::cout << "[" << rank << "]" << "BYE BYE FROM MASTER" << endl;
 }
--- Spawn/slave_src/slave.cpp
+++ Spawn/slave_src/slave.cpp
@@ -14,25 +14,6 @@ MPI_Comm children;
 char processor_name[MPI_MAX_PROCESSOR_NAME];
 int name_len;
 
-void slave_work();
-
-/**
-* @method main
-**/
-int main(int argc, char* argv[])
-{
-  MPI_Init(&argc, &argv);
-  rank = MPI::COMM_WORLD.Get_rank();
-  parent = MPI::COMM_WORLD.Get_parent();
-  MPI_Get_processor_name(processor_name, &name_len);
-
-  slave_work();
-
-  MPI_Finalize();
-
-  return 0;
-}
-
 void slave_work()
 {
   std::cout << "[" << rank << "]" << "HELLO FROM SLAVE @ " << processor_name << endl;
@@ -43,4 +24,23 @@ void slave_work()
     MPI_Send(&n_children, 1, MPI_INT, 0, 0, parent);
 
   std::cout << "[" << rank << "]" << "BYE BYE FROM SLAVE" << endl;
+}
+
+/**
+* @method main
+**/
+int main(int argc, char* argv[])
+{
+  //init
+  MPI_Init(&argc, &argv);
+  rank = MPI::COMM_WORLD.Get_rank();
+  parent = MPI::COMM_WORLD.Get_parent();
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  //work
+  slave_work();
+
+  //finialize
+  MPI_Finalize();
+  return 0;
 }
