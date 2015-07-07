116879eda8409b5280cc1b0e630c34c7eae76eea
rominavarela <rominavarela.mx@gmail.com>
2015-07-07 13:39:54 -0500

    master slvae structure

--- ClientServer/_client/src/client.cpp
+++ ClientServer/_client/src/client.cpp
@@ -32,13 +32,15 @@ int main(int argc, char* argv[])
 	std::cout << "rank:" << world_rank << " , host: " << processor_name << " > Hello World!" << endl;
 
   //init client
+  memset( port_name, '\0', MPI_MAX_PORT_NAME);
   strcpy( port_name, argv[1] );
+  //MPI_Lookup_name("ocean",MPI_INFO_NULL,port_name);
   std::cout << "Initializing connection with server. Port name: " << port_name << endl;
   MPI_Comm_connect( port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &server );
   std::cout << "Server connection up" << endl;
-  /*MPI_Comm_size(server, &server_size);
+  MPI_Comm_size(server, &server_size);
   std::cout << "Server size= " << server_size << endl;
-  */
+
   //run
   run();
 
--- ClientServer/_server/src/server.cpp
+++ ClientServer/_server/src/server.cpp
@@ -30,9 +30,16 @@ int main(int argc, char* argv[])
 	std::cout << "rank:" << world_rank << " , host: " << processor_name << " > Hello World!" << endl;
 
   //init server
-  MPI_Open_port(MPI_INFO_NULL, port_name);
-  std::cout << "Server up. Port name: (" << port_name << ")" << endl;
-  MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF,&client);
+  MPI_Info info;
+  MPI_Info_create(&info);
+  MPI_Info_set(info,"ip_port","1636");
+  //MPI_Info_set(info,"ip_address","10.0.1.103");
+  MPI_Open_port(info, port_name);
+  std::cout << "Server up." << endl;
+  std::cout << "Port name: " << port_name << endl;
+  MPI_Publish_name("ocean", info, port_name);
+  std::cout << "Service name: ocean" << endl;
+  MPI_Comm_accept(port_name, info, 0, MPI_COMM_SELF,&client);
   std::cout << "Client connection acepted" << endl;
 
   //run
@@ -41,6 +48,7 @@ int main(int argc, char* argv[])
   //finalize
   MPI_Comm_disconnect(&client);
   std::cout << "Connection closed. Closing mpi." << endl;
+  MPI_Close_port(port_name);
 	MPI_Finalize();
 	return 0;
 }
